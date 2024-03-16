02ef23663655f93a2f9106ec9b38990202c0677c
ppalmeros <pedrofpalmeros@gmail.com>
2024-03-16 10:55:05 -0600

    [ADD] - Added my window and configure functionality button

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
-
+    Private Sub Btn_Pedro_Click(sender As Object, e As EventArgs) Handles Btn_Pedro.Click
+        Me.Hide()
+        Pedros_window.Show()
+    End Sub
 End Class
---
+++ Pedros_window.vb
@@ -0,0 +1,3 @@
+﻿Public Class Pedros_window
+
+End Class
