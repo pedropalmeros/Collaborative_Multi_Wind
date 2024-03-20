89672cdf8538e50abd46c3c7de8c8052d0ad5999
anaishdorantes <202208058@mondragonmexico.edu.mx>
2024-03-19 22:40:53 -0600

    [Modificación] Btn de regreso a la pantalla principal (Anais a Form1)

--- Anais_Window.Designer.vb
+++ Anais_Window.Designer.vb
@@ -22,9 +22,28 @@ Partial Class Anais_Window
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Anais_Window"
+        Btn_Regresar = New Button()
+        SuspendLayout()
+        ' 
+        ' Btn_Regresar
+        ' 
+        Btn_Regresar.Location = New Point(602, 348)
+        Btn_Regresar.Name = "Btn_Regresar"
+        Btn_Regresar.Size = New Size(94, 29)
+        Btn_Regresar.TabIndex = 0
+        Btn_Regresar.Text = "regresar"
+        Btn_Regresar.UseVisualStyleBackColor = True
+        ' 
+        ' Anais_Window
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Btn_Regresar)
+        Name = "Anais_Window"
+        Text = "Anais_Window"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Btn_Regresar As Button
 End Class
--- Anais_Window.vb
+++ Anais_Window.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Anais_Window
-
+    Private Sub Regresar_Click(sender As Object, e As EventArgs) Handles Btn_Regresar.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
 End Class
